66b269bad51cbb205f88adff6334c67a7ee6346d
Public Happiness Andy <focallocal.london@gmail.com>
2025-11-22 20:02:13 +0000

    Revert "Add composerTemplate field to Route typedef"
    This reverts commit 652a71689604ccfb3ae5478d667f6c945c9a3098.

--- src/com-types.js
+++ src/com-types.js
@@ -9,7 +9,6 @@
  * @property {('COMMENT'|'DISCUSS')} [interactMode] - Only if layout=2|3
  * @property {String} [triggerId] - Only if layout=2|3
  * @property {String} [pathname] - Only if layout=1
- * @property {String} [composerTemplate] - Template ID for url-composer-templates component
  */
 
 /**
